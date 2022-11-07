8cf1645fd8d80c03ee7f1fe9439564108acd475c
Ravi Sawlani <ravisawlani04@gmail.com>
2022-11-07 10:49:41 +0530

    remove TodoTest.jsx

--- test/component/TodoTest.jsx
+++
@@ -1,8 +0,0 @@
-import Todo from '../../src/components/Todo.jsx';
-import { fireEvent, within } from '@testing-library/dom';
-export default {
-  title: 'Todo component',
-  component: Todo
-};
-
-export const TodoComponent = Object.assign(() => <Todo />);
